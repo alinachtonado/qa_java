29ea866010a00c059dc53a7f14d41e36b0486221
Alina Gainetdinova <malvinaalina@yandex.ru>
2022-09-11 15:33:44 +0500

    tests

---
+++ src/test/java/FelineTests.java
@@ -0,0 +1,42 @@
+import com.example.Cat;
+import com.example.Feline;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+public class FelineTests {
+    private Feline feline;
+
+    @Before
+    public void setUp(){
+        feline = new Feline();
+    }
+
+    @Test
+    public void eatMeatTest() throws Exception {
+        List<String> food = feline.eatMeat();
+        assertEquals(Arrays.asList("Животные", "Птицы", "Рыба"), food);
+    }
+
+    @Test
+    public void getFamilyTest() {
+        String family = feline.getFamily();
+        assertEquals("Кошачьи", family);
+    }
+
+    @Test
+    public void getKittensTest() {
+        int kittens = feline.getKittens();
+        assertEquals(1, kittens);
+    }
+
+    @Test
+    public void getKittens2Test() {
+        int kittens = feline.getKittens(11);
+        assertEquals(11, kittens);
+    }
+}
